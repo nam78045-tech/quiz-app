0b6502f94e0820cae94851b772e3ae67918a0c49
Phan Nam <nam78045@gmail.com>
2026-05-09 09:05:59 +0700

    Remove nested quiz-app folder

--- quiz-app/src/main/java/com/quiz/quiz_app/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.quiz_app.repository;
-
-import com.quiz.quiz_app.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-    List<Question> findBySubjectId(Long subjectId);
-}
